0c6e3083196ed81d6c1fef0c131791e308671ca9
hyednjs <joohw1115@naver.com>
2018-08-15 00:15:27 +0900

    map,catlist,catprofile layout

---
+++ app/src/main/java/edu/skku/swp3/straycat/CatList.java
@@ -0,0 +1,13 @@
+package edu.skku.swp3.straycat;
+
+public class CatList {
+    public int imageResId;
+    public String catAddress;
+    public String catSpecies;
+
+    public CatList(int imageResId, String catAddress, String catSpecies) {
+        this.imageResId = imageResId;
+        this.catAddress = catAddress;
+        this.catSpecies = catSpecies;
+    }
+}
--- app/src/main/java/edu/skku/swp3/straycat/TabActivity.java
+++ app/src/main/java/edu/skku/swp3/straycat/TabActivity.java
@@ -1,23 +1,43 @@
 package edu.skku.swp3.straycat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.lang.reflect.Field;
 
-public class TabActivity extends AppCompatActivity {
+
+public class TabActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private View fragmentHolder;
+    private final SupportMapFragment supportMapFragment = new SupportMapFragment();
+    private final DonationMainFragment donationMainFragment = new DonationMainFragment();
+    private static final int MY_LOCATION_REQUEST_CODE = 11;
+
+    private GoogleMap mMap;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -26,16 +46,17 @@ public class TabActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
+
             switch (item.getItemId()) {
                 case R.id.nav_feed:
                     return true;
                 case R.id.nav_map:
-//                    transaction.replace(R.id.nav_fragment, new MapsActivity(), "map");
+                    transaction.replace(R.id.nav_fragment, supportMapFragment, "map").commit();
                     return true;
                 case R.id.nav_plus:
                     return true;
                 case R.id.nav_donation:
-                    transaction.replace(R.id.nav_fragment, new DonationMainFragment(), "donation_main");
+                    transaction.replace(R.id.nav_fragment, donationMainFragment, "donation_main").commit();
                     return true;
                 case R.id.nav_setting:
                     return true;
@@ -60,7 +81,31 @@ public class TabActivity extends AppCompatActivity {
         transaction.add(R.id.nav_fragment, new DonationMainFragment());
         transaction.commit();
         removeShiftMode(navigation);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+        }
+
+        supportMapFragment.getMapAsync(this);
     }
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == MY_LOCATION_REQUEST_CODE) {
+            if (permissions.length == 1 &&
+                    permissions[0] == Manifest.permission.ACCESS_FINE_LOCATION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            } else {
+                // Permission was denied. Display an error message.
+            }
+        }
+    }
+
+
 
     @Override
     public void onBackPressed() {
@@ -68,7 +113,7 @@ public class TabActivity extends AppCompatActivity {
     }
 
     @SuppressLint("RestrictedApi")
-    void removeShiftMode(BottomNavigationView  view) {
+    void removeShiftMode(BottomNavigationView view) {
         BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
         try {
             Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
@@ -88,6 +133,19 @@ public class TabActivity extends AppCompatActivity {
         } catch (IllegalAccessException e) {
             Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode");
         }
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng korea = new LatLng( -122.0840, 37.0000);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(korea));
+
+
+
+
     }
 
 }
